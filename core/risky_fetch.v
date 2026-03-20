b959b311e462ce007074c890f6be3f7e349e875d
imchristina <3430575+imchristina@users.noreply.github.com>
2026-03-20 17:23:18 -0500

    fetch: added logic

--- core/risky_fetch.v
+++ core/risky_fetch.v
@@ -10,15 +10,22 @@ module risky_fetch (
     input [31:0] pc_raddr,
     output pc_inc_en,
 
-    output [31:0] decode_inst_addr,
-    output [31:0] decode_inst_data,
-    output decode_flush_propogate
+    output reg [31:0] decode_inst_addr,
+    output reg [31:0] decode_inst_data,
+    output reg decode_flush_propogate
 );
 
-    assign pc_inc_en = ~(hazard_stall || hazard_flush);
+    assign pc_inc_en = !(hazard_stall || hazard_flush);
+    assign memif_addr = pc_raddr;
 
     always @(posedge clk) begin
-
+        if (!(hazard_stall || hazard_flush)) begin
+            decode_inst_addr <= pc_raddr;
+            decode_inst_data <= memif_rdata;
+            decode_flush_propogate <= 1'd0;
+        end else if (hazard_flush) begin
+            decode_flush_propogate <= 1'd1;
+        end
     end
 
 endmodule
